dd2957e68700b5e00bdb00bf7ca96f230c02ef54
miamiworldwide <rivendalejones@gmail.com>
2017-01-24 20:29:20 -0800

    added addiotional scripts

---
+++ leftGroup.js
@@ -0,0 +1,9 @@
+var inspirations = ['Maywa Denki', 'Box'];
+
+function display(messages){
+  for (var i=0; i < messages.length; i++){
+    console.log(messages[i]);
+  }
+}
+
+display(inspirations);
---
+++ thingsWeLike.js
@@ -0,0 +1,10 @@
+var thingsWeLike = ["1) general solution to a small problem","2) bracket notation able to extract items from the list by number","3) we can use their function to make this list"]
+
+
+function display(messages){
+  for (var i=0; i < messages.length; i++){
+    console.log(messages[i]);
+  }
+}
+
+display(thingsWeLike);
