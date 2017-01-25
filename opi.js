590bf4b38bfe7f59e3b1c3c129d964f7e99411b4
miamiworldwide <rivendalejones@gmail.com>
2017-01-24 19:58:49 -0800

    commit 1

---
+++ opi.js
@@ -0,0 +1,9 @@
+console.log("kaitlyn aurelia smith");
+console.log("https://www.youtube.com/watch?v=9ALnPqBxc6I");
+
+var name = "kaitlyn aurelia smith";
+var count = 0;
+while(count < 21){
+  console.log( name.substr(count,21)) ;
+  count = count +1;
+}
